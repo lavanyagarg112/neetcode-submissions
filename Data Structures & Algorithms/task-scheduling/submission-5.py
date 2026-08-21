5227df4aefc24e243d9c10772bad3dc1454a8bb2
Lavanya Garg <lavanyagarg@u.nus.edu>
2026-08-21 18:39:32 +0800

    Add: task-scheduling - submission-5

---
+++ Data Structures & Algorithms/task-scheduling/submission-5.py
@@ -0,0 +1,42 @@
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+
+        # using queue makes it faster
+        # idea: it is FIFO. if front itself cant be used, 
+        # then others cant
+        
+        heap = [] # minheap
+        freq_map = {}
+
+        for t in tasks:
+            if t not in freq_map:
+                freq_map[t] = 0
+            freq_map[t] += 1
+
+        for t in freq_map:
+            heapq.heappush(heap, (-1 * freq_map[t], t))
+
+        # excluded = {}
+        queue = deque()
+        time = 0
+
+        while heap or queue:
+            time += 1
+            # task is a tuple (rem_freq, task)
+            if queue:
+                reqtime, task = queue.popleft()
+                if reqtime == time:
+                    heapq.heappush(heap, (-1 * task[0], task[1]))
+                else:
+                    queue.appendleft((reqtime, task))
+
+            if not heap:
+                continue
+
+            remaining, next_task = heapq.heappop(heap)
+            remaining = -1 * remaining
+            remaining -= 1
+            if remaining > 0:
+                queue.append((time + n + 1, (remaining, next_task)))
+
+        return time
