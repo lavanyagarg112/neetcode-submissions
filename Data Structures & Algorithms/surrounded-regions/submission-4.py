dd4df87d95f80fe248f87cd8637cd0ce76f47c68
Lavanya Garg <lavanyagarg@u.nus.edu>
2026-08-23 11:48:48 +0800

    Add: surrounded-regions - submission-4

---
+++ Data Structures & Algorithms/surrounded-regions/submission-4.py
@@ -0,0 +1,58 @@
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+
+        # using iterative dfs
+        
+        rows = len(board)
+        cols = len(board[0])
+
+        visited = set()
+
+        def dfs(r, c, convert):
+            stack = [(r,c)]
+
+            while stack:
+                r, c = stack.pop()
+                if r < 0 or c < 0 or r >= rows or c >= cols:
+                    # return
+                    continue
+
+                if (r, c) in visited:
+                    # return
+                    continue
+
+                visited.add((r, c))
+                if board[r][c] == "O":
+                    board[r][c] = convert
+                    stack.append((r-1, c))
+                    stack.append((r+1, c))
+                    stack.append((r, c+1))
+                    stack.append((r, c-1))
+        
+        # first visit all the Os from the edges
+
+        # rows
+        for r in [0, rows-1]:
+            for c in range(cols):
+                if (r,c) not in visited and board[r][c] == "O":
+                    dfs(r, c, "EDGE")
+
+        # cols
+        for r in range(rows):
+            for c in [0, cols-1]:
+                if (r,c) not in visited and board[r][c] == "O":
+                    dfs(r, c, "EDGE")
+
+        # then visit the ones not from the edges and convert them
+        for r in range(rows-1):
+            for c in range(cols-1):
+                if (r,c) not in visited and board[r][c] == "O":
+                    dfs(r, c, "X")
+
+        # then convert EDGE back to O
+        for r in range(rows):
+            for c in range(cols):
+                if board[r][c] == "EDGE":
+                    board[r][c] = "O"
+
+        return
