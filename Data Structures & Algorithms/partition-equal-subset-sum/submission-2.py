972a4b7dc6cce4756de5fc74cc9c801709f7ab54
Lavanya Garg <lavanyagarg@u.nus.edu>
2026-08-24 18:37:09 +0800

    Add: partition-equal-subset-sum - submission-2

---
+++ Data Structures & Algorithms/partition-equal-subset-sum/submission-2.py
@@ -0,0 +1,50 @@
+class Solution:
+
+    def __init__(self):
+        self.truelist = set()
+        self.falselist = set()
+
+    def canFindSubset(self, nums, target):
+        if (target) in self.truelist:
+            # print("memo")
+            return True
+
+        if (target) in self.falselist:
+            # print("memo")
+            return False
+
+        if target == 0:
+            return True
+
+        if len(nums) == 0:
+            return False
+
+        include = False
+        if nums[0] <= target:
+            include = self.canFindSubset(nums[1:], target - nums[0])
+        exclude = self.canFindSubset(nums[1:], target)
+
+        if include or exclude:
+            self.truelist.add((target))
+            return True
+
+        self.falselist.add((target))
+        return False
+
+
+    def canPartition(self, nums: List[int]) -> bool:
+        
+        total = sum(nums)
+
+        # cannot even get equal sum
+        if total % 2 == 1:
+            return False
+
+        target = total//2
+
+        nums = tuple(nums)
+
+        if self.canFindSubset(nums, target):
+            return True
+
+        return False
