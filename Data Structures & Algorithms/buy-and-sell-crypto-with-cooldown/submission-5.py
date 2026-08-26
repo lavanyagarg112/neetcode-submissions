5105a7ece207b140fe3d567a40d3b800c8269e68
Lavanya Garg <lavanyagarg@u.nus.edu>
2026-08-27 07:59:27 +0800

    Add: buy-and-sell-crypto-with-cooldown - submission-5

---
+++ Data Structures & Algorithms/buy-and-sell-crypto-with-cooldown/submission-5.py
@@ -0,0 +1,27 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+
+        # top down -> second approach
+        # add price if selling, subtract price if buying
+        
+        memo = {}
+
+        def dp(canBuy, today):
+            if today >= len(prices):
+                return 0
+
+            if (canBuy, today) in memo:
+                return memo[(canBuy, today)]
+
+            ans = dp(canBuy, today+1) # do nothing on this day
+            if not canBuy:
+                ans = max(ans, dp(True, today+2) + prices[today]) # sell today
+            else:
+                ans = max(ans, dp(False, today+1) - prices[today]) # buy today
+
+            memo[(canBuy, today)] = ans
+
+            return ans
+
+        ans = dp(True, 0)
+        return ans
