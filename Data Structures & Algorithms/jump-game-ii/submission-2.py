f65535a30be60aa95da51f8be3b78e36940c98d9
Lavanya Garg <lavanyagarg@u.nus.edu>
2026-09-03 15:17:11 +0800

    Add: jump-game-ii - submission-2

---
+++ Data Structures & Algorithms/jump-game-ii/submission-2.py
@@ -0,0 +1,25 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        
+        # O(n)
+        # go forward
+        # keep getting the farthest we can go in current window
+        # like bfs
+
+        jumps = 0
+        left = 0
+        right = 0
+
+        while right < len(nums) - 1:
+            farthest = 0 # right now i cant go anywhere
+            for i in range(left, right + 1):
+                # see how far i can go in my current window
+                # with just one more jump
+                farthest = max(farthest, i + nums[i])
+
+            # next window
+            left = right + 1
+            right = farthest
+            jumps += 1
+        
+        return jumps
