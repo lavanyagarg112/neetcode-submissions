95832d0a4006caf89a8947587b818d91c7806a3c
Lavanya Garg <lavanyagarg@u.nus.edu>
2026-09-15 10:28:30 +0800

    Add: count-squares - submission-4

---
+++ Data Structures & Algorithms/count-squares/submission-4.py
@@ -0,0 +1,56 @@
+class CountSquares:
+
+    def __init__(self):
+        self.points = {}
+        
+
+    def add(self, point: List[int]) -> None:
+        # O(1)
+        if (point[0], point[1]) not in self.points:
+            self.points[(point[0], point[1])] = 0
+        self.points[(point[0], point[1])] += 1
+        return
+        
+
+    def count(self, point: List[int]) -> int:
+        # O(n^2)
+        # so anything where the x is on the left/right
+        # with a valid y
+        # the y is on the top/bottom with valid x
+        # and the intersecting point of these two on the other
+        # side
+
+        currx = point[0]
+        curry = point[1]
+
+        count = 0
+
+        possible_x_coord = set()
+        possible_y_coord = set()
+
+        for x, y in self.points:
+            if x == currx and y == curry:
+                continue
+
+            if y == curry:
+                # one point on x axis
+                possible_x_coord.add(x)
+
+            if x == currx:
+                possible_y_coord.add(y)
+
+        # if the intersection is there
+        for x in possible_x_coord:
+            for y in possible_y_coord:
+                if (x, y) in self.points:
+                    # check for square
+                    if abs(x - currx) != abs(y - curry):
+                        continue
+                    curr_count = self.points[(x, curry)] * self.points[(currx, y)] * self.points[(x, y)]
+                    count += curr_count
+
+
+        return count
+
+
+        
